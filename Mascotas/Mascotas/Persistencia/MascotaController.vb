e0bdd71c576493f60c31d5ea956560f177b074dd
lemajusi <lmartinez16access@gmail.com>
2020-08-05 09:30:36 -0300

    Mascota
    Persistencia

--- Mascotas/Mascotas/Persistencia/MascotaController.vb
+++ Mascotas/Mascotas/Persistencia/MascotaController.vb
@@ -1,3 +1,11 @@
 ﻿Public Class MascotaController
+    Dim conection_Npg = New Npgsql.NpgsqlConnection
+    Public Sub altaMascota()
+        Dim conection = New Conextion
+        Dim cmd = New Npgsql.NpgsqlCommand
+        conection_Npg = conection.AbrirConextion
+        cmd.Connection = conection_Npg
+        Dim CadenaDeComandos = "insert into mascota (ci, nombre, direccion) values (@ci, @nombre, @direccion, @telefono)"
 
+    End Sub
 End Class
--- Mascotas/Mascotas/Persistencia/PersonaController.vb
+++ Mascotas/Mascotas/Persistencia/PersonaController.vb
@@ -1,20 +1,16 @@
 ﻿Public Class PersonaController
     Dim conection_Npg = New Npgsql.NpgsqlConnection
-    Sub New()
-
-    End Sub
     Public Sub altaPersona(personaUser As ClassPersona)
         Try
             Dim conection = New Conextion
-            Dim cmd = New Npgsql.NpgsqlCommand
             conection_Npg = conection.AbrirConextion
-            cmd.Connection = conection_Npg
             Dim CadenaDeComandos = "insert into persona (ci, nombre, direccion) values (@ci, @nombre, @direccion, @telefono)"
+            Dim cmd = New Npgsql.NpgsqlCommand(CadenaDeComandos)
+            cmd.Connection = conection_Npg
             cmd.Parameters.Add("@ci", NpgsqlTypes.NpgsqlDbType.Integer).Value = personaUser.cedula
             cmd.Parameters.Add("@nombre", NpgsqlTypes.NpgsqlDbType.Varchar, 500).Value = personaUser.nombre
             cmd.Parameters.Add("@dirreccion", NpgsqlTypes.NpgsqlDbType.Varchar, 500).Value = personaUser.direccion
-            cmd.Parameters.Add("@telefono", NpgsqlTypes.NpgsqlDbType.Varchar, )
-
+            cmd.Parameters.Add("@telefono", NpgsqlTypes.NpgsqlDbType.Varchar, 500).Value = personaUser.telefono
             Dim resultado As Integer = cmd.ExecuteNonQuery()
         Catch ex As Exception
             Throw ex
