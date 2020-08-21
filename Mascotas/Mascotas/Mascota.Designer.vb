e75c7e338a924de23b9c9b8b1b89cb15bd7df36d
lemajusi <lmartinez16access@gmail.com>
2020-08-21 12:56:09 -0300

    commit mascota

--- Mascotas/Mascotas/Mascota.Designer.vb
+++ Mascotas/Mascotas/Mascota.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Mascota
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Mascota))
+        Me.Label12 = New System.Windows.Forms.Label()
         Me.AñoNacMas = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.NombreMas = New System.Windows.Forms.TextBox()
@@ -33,17 +34,26 @@ Partial Class Mascota
         Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(149, 76)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(47, 13)
+        Me.Label12.TabIndex = 32
+        Me.Label12.Text = "mascota"
+        '
         'AñoNacMas
         '
-        Me.AñoNacMas.Location = New System.Drawing.Point(82, 173)
+        Me.AñoNacMas.Location = New System.Drawing.Point(130, 165)
         Me.AñoNacMas.Name = "AñoNacMas"
-        Me.AñoNacMas.Size = New System.Drawing.Size(190, 20)
+        Me.AñoNacMas.Size = New System.Drawing.Size(137, 20)
         Me.AñoNacMas.TabIndex = 31
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(79, 157)
+        Me.Label6.Location = New System.Drawing.Point(79, 168)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(45, 13)
         Me.Label6.TabIndex = 30
@@ -51,15 +61,15 @@ Partial Class Mascota
         '
         'NombreMas
         '
-        Me.NombreMas.Location = New System.Drawing.Point(82, 95)
+        Me.NombreMas.Location = New System.Drawing.Point(130, 113)
         Me.NombreMas.Name = "NombreMas"
-        Me.NombreMas.Size = New System.Drawing.Size(190, 20)
+        Me.NombreMas.Size = New System.Drawing.Size(137, 20)
         Me.NombreMas.TabIndex = 29
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(79, 79)
+        Me.Label4.Location = New System.Drawing.Point(80, 116)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(44, 13)
         Me.Label4.TabIndex = 28
@@ -68,16 +78,15 @@ Partial Class Mascota
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(113, 24)
+        Me.Label1.Location = New System.Drawing.Point(154, 35)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(143, 37)
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
         Me.Label1.TabIndex = 33
-        Me.Label1.Text = "Registro"
+        Me.Label1.Text = "Ingreso"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(144, 243)
+        Me.Button1.Location = New System.Drawing.Point(131, 211)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 34
@@ -86,15 +95,15 @@ Partial Class Mascota
         '
         'CedulaPer
         '
-        Me.CedulaPer.Location = New System.Drawing.Point(82, 134)
+        Me.CedulaPer.Location = New System.Drawing.Point(131, 139)
         Me.CedulaPer.Name = "CedulaPer"
-        Me.CedulaPer.Size = New System.Drawing.Size(190, 20)
+        Me.CedulaPer.Size = New System.Drawing.Size(137, 20)
         Me.CedulaPer.TabIndex = 36
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(79, 118)
+        Me.Label2.Location = New System.Drawing.Point(81, 142)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(47, 13)
         Me.Label2.TabIndex = 35
@@ -104,24 +113,25 @@ Partial Class Mascota
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(353, 291)
+        Me.ClientSize = New System.Drawing.Size(353, 253)
         Me.Controls.Add(Me.CedulaPer)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label12)
         Me.Controls.Add(Me.AñoNacMas)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.NombreMas)
         Me.Controls.Add(Me.Label4)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MaximumSize = New System.Drawing.Size(369, 330)
-        Me.MinimumSize = New System.Drawing.Size(369, 330)
         Me.Name = "Mascota"
         Me.Text = "Mascota"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Label12 As Label
     Friend WithEvents AñoNacMas As TextBox
     Friend WithEvents Label6 As Label
     Friend WithEvents NombreMas As TextBox
